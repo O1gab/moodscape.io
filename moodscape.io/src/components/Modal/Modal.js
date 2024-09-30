6f8c65b7683227cc71db8d05c5f677e9465f2df4
Olga Batiunia <olgabatiunia@gmail.com>
2024-09-30 04:26:15 +0200

    modal: labels changed, design changed

--- moodscape.io/src/components/Modal/Modal.js
+++ moodscape.io/src/components/Modal/Modal.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Modal.css';
 
 const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay">
-      <div className="modal-content">
+    <div className={`modal-overlay ${isOpen ? 'open' : ''}`}>
+        <div className="modal-content">
+            <button className="close-button" onClick={onClose}>
+                &times;
+            </button>
         <h2>Almost There!</h2>
-        <p>The app is currently under moderation. Sign up to get notified when MoodScape is available!</p>
-        <button className="close-button" onClick={onClose}>Close</button>
+        <p>MoodScape is currently under moderation and will be launched soon (mid-October). Sign up to get notified once MoodScape is available!</p>
+        <form className="email-form">
+          <input type="email" className="email-input" placeholder="Enter your email" required />
+          <button type="submit" className="cta-button">Notify Me</button>
+        </form>
       </div>
     </div>
   );
