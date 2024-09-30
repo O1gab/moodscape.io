662baa6972a430a2b7bd4f76615c49b5e401ef3d
Olga Batiunia <olgabatiunia@gmail.com>
2024-10-01 01:48:22 +0200

    server: firestore added, emails can be sent, bugs fixed

--- moodscape.io/src/server/server.js
+++ moodscape.io/src/server/server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer'); // For sending emails
-require('dotenv').config(); // Load environment variables from .env file
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 const cors = require('cors');
+const admin = require('firebase-admin');
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const serviceAccount = require('./serviceAccountKey.json'); // Use the correct path
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://moodscape-io.firebaseio.com"
+});
+
+const db = admin.firestore();
+
 // Configure your email transport
 let transporter = nodemailer.createTransport({
-  service: 'Gmail', // or any other email service
+  host: 'mail.privateemail.com', port: 465, secure: true,
   auth: {
-    user: process.env.EMAIL_USER, // Use environment variable
-    pass: process.env.EMAIL_PASS, // Use environment variable
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
@@ -23,21 +34,32 @@ app.post('/subscribe', async (req, res) => {
     return res.status(400).send('Email is required');
   }
 
-  // Send email confirmation (you can modify the content as needed)
-  let mailOptions = {
-    from: 'no-reply@moodscape.io',
-    to: email,
-    subject: 'Thanks for subscribing!',
-    text: 'Thank you for signing up for MoodScape updates!',
-    html: '<b>Thank you for signing up for MoodScape updates!</b>',
-  };
+   try {
+    const snapshot = await db.collection('subscribers')
+      .where('email', '==', email)
+      .get();
 
-  try {
+    if (!snapshot.empty) {
+      // Email already exists, send a 409 status (Conflict)
+      return res.status(409).json({ message: 'Email already exists' });
+    }
+
+    await db.collection('subscribers').add({ email });
+
+    // Send email confirmation
+    let mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Thanks for subscribing!',
+      text: 'Thank you for signing up for MoodScape updates!',
+      html: '<b>Thank you for signing up for MoodScape updates!</b>',
+    };
+
     await transporter.sendMail(mailOptions);
     res.status(200).send('Subscription successful');
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error sending email');
+    res.status(500).send('Error processing subscription');
   }
 });
 
