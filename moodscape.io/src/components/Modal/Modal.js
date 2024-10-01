435393fdbdb2f567ac960ae5cb10744ef165c882
Olga Batiunia <olgabatiunia@gmail.com>
2024-10-01 02:06:34 +0200

    modal: ui changed, bugs fixed, email functionality added

--- moodscape.io/src/components/Modal/Modal.js
+++ moodscape.io/src/components/Modal/Modal.js
@@ -4,6 +4,7 @@ import './Modal.css';
 const Modal = ({ isOpen, onClose }) => {
   const [email, setEmail] = useState('');
   const [submitted, setSubmitted] = useState(false);
+  const [submittedEmail, setSubmittedEmail] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   if (!isOpen) return null;
@@ -11,7 +12,7 @@ const Modal = ({ isOpen, onClose }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     
-    const emailToSubmit = email; // Get the email from state
+    const emailToSubmit = email;
 
     try {
       const response = await fetch('http://localhost:3001/subscribe', {
@@ -19,13 +20,14 @@ const Modal = ({ isOpen, onClose }) => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email: emailToSubmit }), // Submit the email from state
+        body: JSON.stringify({ email: emailToSubmit }),
       });
 
       if (response.status === 409) {
       // If the response status is 409 (Conflict), the email already exists
         const data = await response.json();
-        setErrorMessage(data.message); // Set the error message
+        setErrorMessage(data.message);
+        setSubmitted(false);
         return;
       }
       
@@ -33,12 +35,10 @@ const Modal = ({ isOpen, onClose }) => {
         throw new Error('Network response was not ok');
       }
 
-      const data = await response.json();
-      console.log(data);
-      
-      setSubmitted(true); // Update the submitted state to true
-      setEmail(''); // Clear the email input after submission
-      setErrorMessage(''); // Clear error message on success
+      setSubmittedEmail(emailToSubmit)
+      setEmail('');
+      setErrorMessage('');
+      setSubmitted(true); 
     } catch (error) {
       console.error('Error:', error);
       setErrorMessage('Something went wrong. Please try again.');
@@ -53,27 +53,31 @@ const Modal = ({ isOpen, onClose }) => {
         </button>
         <h2>Almost There!</h2>
         <p>MoodScape is currently under moderation and will be launched soon (mid-October). Sign up to get notified once MoodScape is available!</p>
-        {!submitted ? (
-          <form className="email-form" onSubmit={handleSubmit}>
-            <input
-              type="email"
-              className="email-input"
-              placeholder="Enter your email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <button type="submit" className="cta-button">Notify Me</button>
-          </form>
+         {!submitted ? (
+          <>
+            {/* Show error message if any */}
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+            
+            <form className="email-form" onSubmit={handleSubmit}>
+              <input
+                type="email"
+                className="email-input"
+                placeholder="Enter your email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <button type="submit" className="cta-button">Notify Me</button>
+            </form>
+          </>
         ) : (
           <div>
-            <p>Thanks for signing up!</p>
-            <p>A confirmation email has been sent to {email}.</p> {/* Show the email */}
+            <p className="success-message">Thanks for signing up!</p>
+            <p className="success-message">A confirmation email has been sent to {submittedEmail}.</p> {/* Display success message with email */}
           </div>
         )}
       </div>
     </div>
   );
 };
-
 export default Modal;
